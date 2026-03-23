53a97a42a8e453eb4c1888bc0b47600637148415
ao <mc4@outlook.jp>
2026-03-24 01:45:31 +0900

    r7/mon2_a/

--- r7/mon2_a/mon2_a.ino
+++ r7/mon2_a/mon2_a.ino
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #define USE_TIMER3_ISR
 #include "mono_con.h"
 
@@ -42,4 +43,50 @@ void loop() {
     lm.color.GBR = ledBlank;
   }
   led_stepmotor(lm.b8);
+=======
+#define USE_TIMER3_ISR
+#include "mono_con.h"
+
+const int D3 = 19;
+
+int TSW = 0;
+int ledBlank = B000;
+int ledParam[] = { B001, B100, B010 };
+int ledValue = 0;
+bool prev_on = false;
+
+word in, led;
+
+ISR (TIMER3_COMPA_vect) {
+  if (in > 5) {
+    in = 0;
+    TSW = digitalRead(D3);
+  }
+  in++;
+  led++;
+}
+
+void setup() {
+  config_init();
+  serial_init();
+}
+
+void loop() {
+  if (TSW == HIGH) {
+    prev_on = true;
+    if (led > 1000) {
+      led = 0;
+      lm.color.GBR = ledParam[ledValue];
+      if (++ledValue > 2) ledValue = 0;
+    }
+  } else {
+    if (prev_on) {
+      if (--ledValue < 0) ledValue = 2;
+      prev_on = false;
+    }
+    led = 1000;
+    lm.color.GBR = ledBlank;
+  }
+  led_stepmotor(lm.b8);
+>>>>>>> ef3547c (r7/mon2_a/)
 }
