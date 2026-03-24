7df6f7fe8ebb8d17166d933858eac22673320ed4
ªº <mc4@outlook.jp>
2026-03-24 06:46:36 +0000

    r7/

--- r7/mon3_a/mon3_a.ino
+++ r7/mon3_a/mon3_a.ino
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #define USE_TIMER3_ISR
 #include "mono_con.h"
 
@@ -46,53 +45,4 @@ void loop() {
   } else {
     DMstop();
   }
-=======
-#define USE_TIMER3_ISR
-#include "mono_con.h"
-
-const int y_pin = A1;
-
-int y_pos = 0;
-
-word in;
-
-void setup() {
-  config_init();
-  serial_init();
-
-  pinMode(A2, INPUT);
-}
-
-ISR (TIMER3_COMPA_vect) {
-  if (in > 5) {
-    in = 0;
-    y_pos = analogRead(y_pin);
-  }
-  in++;
-}
-
-void DMcw(int spd) {
-  analogWrite(FIN_PIN, spd);
-  digitalWrite(RIN_PIN, LOW);
-}
-
-void DMccw(int spd) {
-  digitalWrite(FIN_PIN, LOW);
-  analogWrite(RIN_PIN, spd);
-}
-
-void DMstop() {
-  digitalWrite(FIN_PIN, HIGH);
-  digitalWrite(RIN_PIN, HIGH);
-}
-
-void loop() {
-  if (y_pos < 320) {
-    DMccw(100);
-  } else if (y_pos > 780) {
-    DMcw(100);
-  } else {
-    DMstop();
-  }
->>>>>>> 9bf2f4e (r7/mon3_a/)
 }
--- r7/mon4_a/mon4_a.ino
+++ r7/mon4_a/mon4_a.ino
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #define USE_TIMER3_ISR
 #include "mono_con.h"
 
@@ -30,37 +29,4 @@ void loop() {
     disp(num[ten], num[one]);
   }
   tone(BZ_PIN, bz_pitch[bz_index]);
-=======
-#define USE_TIMER3_ISR
-#include "mono_con.h"
-
-void setup() {
-  config_init();
-  serial_init();
-  noTone(BZ_PIN);
-  disp(num[0], num[0]);
-}
-
-int bz_pitch[] = { 1000, 500 };
-int bz_index = 0, cnt = 0;
-int ten = 0, one = 0;
-word tc;
-
-ISR (TIMER3_COMPA_vect) {
-  tc++;
-}
-
-void loop() {
-  if (tc > 1000) {
-    tc = 0;
-    if (++bz_index > 1) bz_index = 0;
-    
-    if (++cnt > 99) cnt = 0;
-    ten = cnt / 10;
-    one = cnt % 10;
-
-    disp(num[ten], num[one]);
-  }
-  tone(BZ_PIN, bz_pitch[bz_index]);
->>>>>>> 91b3e08 (r7/mon4_a/)
 }
