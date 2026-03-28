ea3c88deca727d4b517a433ddb0a62a5a784fddb
ao <mc4@outlook.jp>
2026-03-29 00:14:43 +0900

    r6/mon3/mon3.ino

--- r6/mon3/mon3.ino
+++ r6/mon3/mon3.ino
@@ -1,47 +1,48 @@
 #define USE_TIMER3_ISR
 #include "mono_con.h"
 
-const int D1 = 18;
-const int D2 = 19;
-const int D3 = 17;
-
-int TSW = 0, SW1 = 0, SW2 = 0;
-int pTSW = 0, pSW1 = 0, pSW2 = 0;
-
-bool mode = true;
-
+const int a2[2] = { 0x77, 0x5B };
+const int BLANK = 0x00;
 const int ptn[4][2] = {
 	{ 0x58, 0x61 },
 	{ 0x54, 0x23 },
 	{ 0x23, 0x54 },
-	{ 0x43, 0x4c },
+	{ 0x43, 0x4C },
 };
 
-int ptnidx = 0, dir = 1;
+int idx = 0, dir = 1;
+bool status = false;
+bool tick = false;
+bool idxChanged = false;
 
-word in, tc;
+int TSW = HIGH, SW1 = HIGH, SW2 = HIGH;
+int preTSW = HIGH, preSW1 = HIGH, preSW2 = HIGH;
 
-ISR (TIMER3_COMPA_vect) {
+word in = 0, wait = 0;
+
+ISR(TIMER3_COMPA_vect) {
 	if (in > 5) {
 		in = 0;
-
-		pTSW = TSW, pSW1 = SW1, pSW2 = SW2;
-
-		TSW = digitalRead(D1);
-		SW1 = digitalRead(D2);
-		SW2 = digitalRead(D3);
+		TSW = digitalRead(18);
+		SW1 = digitalRead(19);
+		SW2 = digitalRead(17);
 	}
-	
-	if (tc > 500) {
-		tc = 0;
-		disp(ptn[ptnidx][0], ptn[ptnidx][1]);
-		if (++ptnidx > 3) ptnidx = 0;
-	}
-
 	in++;
-	
-	if (!mode) tc = 500;
-	else tc++;
+
+	if (status) {
+		if (++wait > 500) {
+			wait = 0;
+			tick = true;
+
+			idx += dir;
+			if (idx <= 0 || idx >= 3) dir = -dir;
+			idx = (idx < 0) ? 0 : (idx > 3) ? 3 : idx;
+			idxChanged = true;
+		}
+	} else {
+		wait = 0;
+		tick = false;
+	}
 }
 
 void setup() {
@@ -50,41 +51,67 @@ void setup() {
 }
 
 void loop() {
-	if (TSW == HIGH) {
-		if (SW1 == LOW) {
-			if (SW2 == LOW) {
-				disp(0x77, num[2]);
-			} else {
-				disp(0x77, num[10]);
-			}
+	int tsw = TSW;
+	int sw1 = SW1;
+	int sw2 = SW2;
+	bool sts = status;
+	bool tck = tick;
+	bool idxC = idxChanged;
+	int  cidx = idx;
+	tick = false;
+	idxChanged = false;
 
-		} else {
-			if (SW2 == LOW) {
-				disp(num[10], num[2]);
-			} else {
-				disp(num[10], num[10]);
-			}
+	if (tsw == HIGH) {
+		if (tsw != preTSW) disp(BLANK, BLANK);
+		if (sw1 != preSW1 || sw2 != preSW2) {
+			if (sw1 == LOW && sw2 == LOW)  disp(a2[0], a2[1]);
+			else if (sw1 == LOW && sw2 == HIGH) disp(a2[0], BLANK);
+			else if (sw1 == HIGH && sw2 == LOW)  disp(BLANK, a2[1]);
+			else                                 disp(BLANK, BLANK);
 		}
 
 	} else {
-		if (TSW != pTSW) mode = false;
-
-		if (!mode && SW1 == LOW && pSW1 == HIGH) {
-			ptnidx = 0;
+		if (tsw != preTSW) {
+			status = false;
+			idx = 0;
 			dir = 1;
-			mode = true;
-		}
-
-		if (mode && SW2 == LOW && pSW2 == HIGH) {
-			mode = false;
-		}
-
-		if (mode) {
-
+			wait = 0;
+			tick = false;
+			sts = false;
+			cidx = 0;
+			disp(BLANK, BLANK);
 
 		} else {
-			disp(0x00, 0x00);
+			if (!sts && sw1 == LOW && preSW1 == HIGH) {
+				status = true;
+				idx = 0;
+				dir = 1;
+				wait = 0;
+				sts = true;
+				cidx = 0;
+				disp(ptn[0][0], ptn[0][1]);
+			}
 
+			if (sts && sw2 == LOW && preSW2 == HIGH) {
+				status = false;
+				idx = 0;
+				dir = 1;
+				wait = 0;
+				sts = false;
+				disp(BLANK, BLANK);
+			}
+
+			if (sts && idxC) {
+				disp(ptn[cidx][0], ptn[cidx][1]);
+			}
+
+			if (!sts) {
+				disp(BLANK, BLANK);
+			}
 		}
 	}
+
+	preTSW = tsw;
+	preSW1 = sw1;
+	preSW2 = sw2;
 }
